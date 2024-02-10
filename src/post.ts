de9ddafe905e0bb15d35a07056e6322154168212
Callum Cunha <callum.cunha@gmail.com>
2024-02-10 20:48:31 +0000

    Add file existence check before artifact upload

--- src/post.ts
+++ src/post.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import { exec } from '@actions/exec'
+import fs from 'node:fs'
 import { getInput } from './util'
 import { DefaultArtifactClient } from '@actions/artifact'
 
@@ -11,16 +12,23 @@ export async function run(): Promise<void> {
   try {
     core.info('Beginning post OpenVPN cleanup')
     exec('sudo killall', ['openvpn'])
-    exec('sudo chmod', ['777', getInput('log-filepath')])
 
-    const artifactName = getInput('log-save-as')
-    if (artifactName) {
-      const split = getInput('log-filepath').split('/')
-      const filename = split[split.length - 1]
-      const path = getInput('log-filepath').replace(filename, '')
+    if (fs.existsSync(getInput('log-filepath'))) {
+      exec('sudo chmod', ['777', getInput('log-filepath')])
 
-      const client = new DefaultArtifactClient()
-      await client.uploadArtifact(artifactName, [filename], path)
+      const artifactName = getInput('log-save-as')
+      if (artifactName) {
+        const split = getInput('log-filepath').split('/')
+        const filename = split[split.length - 1]
+        const path = getInput('log-filepath').replace(filename, '')
+
+        const client = new DefaultArtifactClient()
+        await client.uploadArtifact(artifactName, [filename], path)
+      }
+    } else if (getInput('log-save-as')) {
+      core.warning(
+        `The file ${getInput('log-filepath')} does not exist. Skipping artifact upload`
+      )
     }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
